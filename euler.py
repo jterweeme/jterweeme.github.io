9ed543a90b88518432d0377be3fa6892cae6b19b
Jasper ter Weeme <jasper@yogalinux.terweeme.eu>
2018-06-21 20:30:54 +0200

    euler #33

--- euler.py
+++ euler.py
@@ -2182,13 +2182,13 @@ Antwoord: 100
 """
 
 def opdracht33():
-	d = 1
-	for i in range(1, 10):
-		for j in range(1, i):
-			q, r = divmod(9*j*i, 10*j-i)
-			if not r and q <= 9:
-				d*= i/float(j)
-	return int(d)
+    d = 1
+    for i in range(1, 10):
+        for j in range(1, i):
+            q, r = divmod(9*j*i, 10*j-i)
+            if not r and q <= 9:
+                d*= i/j
+    return int(d)
 
 """
 #34: Digit factorials
