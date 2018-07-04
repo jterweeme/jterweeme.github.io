1551521bea3ceab4f9fd6d22f082464625d5458b
Jasper ter Weeme <jasper@yogalinux.terweeme.eu>
2018-07-04 09:45:11 +0200

    euler #51 javascript

--- euler.py
+++ euler.py
@@ -3301,6 +3301,8 @@ this family, is the smallest prime with this property.
 Find the smallest prime which, by replacing part of the number (not
 necessarily adjacent digits) with the same digit, is part of an eight
 prime value family.
+
+Antwoord: 121,313
 """
 
 def opdracht51():
