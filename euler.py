670c161698723418dfab98e3d16a65e57be7cd77
Jasper ter Weeme <jasper@yogalinux.terweeme.eu>
2018-08-04 15:45:09 +0200

    euler #73 python

--- euler.py
+++ euler.py
@@ -2666,8 +2666,8 @@ Adapted from:
 https://www.mathblog.dk/project-euler-71-proper-fractions-ascending-order/
 """
 
-def problem71():
-    a, b, r, s, limit = 3,7,0,1,10**6
+def problem71(limit = 10**6):
+    a, b, r, s = 3,7,0,1
     q = limit
     while q > 2:
         p = (a * q - 1) // b
@@ -2676,11 +2676,48 @@ def problem71():
         q -= 1
     return r
 
-def problem72():
-    return 0
+"""
+#72: Counting fractions
+
+Antwoord: 303963552391
+"""
+
+def problem72(L = 10**6):
+    phi = list(range(L + 1))
+    for n in range(2, L + 1):
+        if phi[n] == n:
+            for k in range(n, L + 1, n):
+                phi[k] -= phi[k] // n
+    return sum(phi) - 1
+
+"""
+#73: Counting fractions in a range
+
+Consider the fraction, n/d, where n and d are positive integers.
+If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
+
+If we list the set of reduced proper fractions
+for d ≤ 8 in ascending order of size, we get:
+
+1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7,
+1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+
+It can be seen that there are 3 fractions between 1/3 and 1/2.
+
+How many fractions lie between 1/3 and 1/2 in the
+sorted set of reduced proper fractions for d ≤ 12,000?
+"""
+
+def rpf(d):
+    for n in range(1,d):
+        if gcd(n, d) == 1: yield (n, d)
 
 def problem73():
-    return 0
+    xcount = 0
+    for d in range(2, 12001):
+        for xrpf in rpf(d):
+            if xrpf[0] / xrpf[1] > 1/3 and xrpf[0] / xrpf[1] < 1/2: xcount += 1
+    return xcount
 
 def problem74():
     return 0
@@ -2804,7 +2841,7 @@ answers = [233168, 4613732, 6857, 906609, 232792560, 25164150, 104743, 235146240
     932718654, 840, 210, 7652413, 162, 16695334890, 5482660, 1533776805, 5777,
     134043, 9110846700, 296962999629, 997651, 121313, 142857, 4075, 376, 249, 972,
     153, 26241, 107359, 26033, 28684, 127035954683, 49, 1322, 272, 661, 7273,
-    6531031914842725, 510510, 8319823, 428570, 0, 0, 0, 0, 381138582]
+    6531031914842725, 510510, 8319823, 428570, 303963552391, 7295372, 0, 0, 381138582]
 
 #answers[61 - 1] = 0
 
