778d91f01fe8f853bfe0e5dd0f47c2a4c6bbf183
Jasper ter Weeme <jasper@yogalinux.terweeme.eu>
2018-09-24 20:05:03 +0200

    euler.cs #17

--- euler.cs
+++ euler.cs
@@ -639,8 +639,73 @@ Antwoord: 21,124
 
 static UInt32 problem17()
 {
-    return 0;
+    string[] arr = new string[] { "one", "two", "three", "four", "five", "six",
+        "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen",
+        "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+        "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty",
+        "ninety"};
+    int xsum = 0;
+    for (UInt32 i = 0; i < 19; i++) xsum += arr[i].Length;
+    for (UInt32 pre = 19; pre < 27; pre++)
+    {   xsum += arr[pre].Length;
+        for (UInt32 word = 0; word < 9; word++)
+            xsum += arr[pre].Length + arr[word].Length;
+    }
+    for (UInt32 a = 0; a < 9; a++)
+    {   xsum += arr[a].Length + "hundred".Length;
+        for (UInt32 b = 0; b < 9; b++)
+            xsum += arr[a].Length + "hundred".Length + "and".Length + arr[b].Length;
+        for (UInt32 b = 9; b < 19; b++)
+            xsum += arr[a].Length + "hundred".Length + "and".Length + arr[b].Length;
+        for (UInt32 c = 19; c < 27; c++)
+        {   xsum += arr[a].Length + "hundred".Length + "and".Length + arr[c].Length;
+            for (UInt32 d = 0; d < 9; d++)
+                xsum += arr[a].Length + "hundred".Length + "and".Length +
+                    arr[c].Length + arr[d].Length;
+        }
+    }
+    xsum += "one".Length + "thousand".Length;
+    return (UInt32)xsum;
 }
+
+/*
+#18: Maximum path sum I
+
+By starting at the top of the triangle below and moving to adjacent
+numbers on the row below, the maximum total from top to bottom is 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+
+That is, 3 + 7 + 4 + 9 = 23.
+
+Find the maximum total from top to bottom of the triangle below:
+
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+NOTE: As there are only 16384 routes, it is possible to solve this
+problem by trying every route. However, Problem 67, is the same
+challenge with a triangle containing one-hundred rows; it cannot be
+solved by brute force, and requires a clever method! ;o)
+
+Antwoord: 1,074
+*/
 
 static UInt32 problem18()
 {
