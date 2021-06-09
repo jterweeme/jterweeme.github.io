37d124ff10b9631db68889bcd62e40579268f2fa
Jasper ter Weeme <jterweeme@gmail.com>
2021-06-09 21:48:57 +0200

    rt2.php

---
+++ rt2.php
@@ -0,0 +1,47 @@
+Ich bin ein berliner
+
+The close of WWII has divided Germany into
+
+Bronze: Deliver 8 loads of FOOD, 5 loads of
+DIESEL and 4 loads of TROOPS to Berlin.
+
+Silver: Deliver the above, plus 3 loads of
+STEEL.
+
+Gold: Deliver the above, plus 2 loads of
+AMMUNITION and 2 loads of WEAPONS.
+
+Linz: 1x steel
+Munich: 2x weapons
+Stuttgart: 2x weapons
+Wurzburg Junction
+Wurzburg: 1x troops
+Wurzburg Crossing
+Wurzburg Place
+Nurnburg: 1x troops
+Augsburg: 2x food
+Ulm
+Manheim: 2x food
+Karlsruhe: 1x troops
+Strasbourg: 2x diesel
+Saarbrucken: 1x steel
+Cologne: 1x diesel
+Duisberg: 1x steel
+Dortmund
+Osnabruck: 2x food
+Bremen: 1x troops
+Bremerhaven: 2x diesel
+Hamburg: 1x ammo
+Schwerin Junction
+Gottingen Crossing
+Gera Junction
+BERLIN
+Hannover: 2x food
+Salzburg Junction
+Mainz: 1x ammo
+Kiel: 2x food
+Ingolstadt: 1x diesel
+Frankfurt: 1x troops
+
+
+
